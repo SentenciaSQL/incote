4f4fd7479e7cf5bb427d6da15936c28f7a57e782
SentenciaSQL <inf512.andres.frias@gmail.com>
2021-06-22 20:03:55 -0400

    first commit

---
+++ wp-content/uploads/custom-css-js/830.js
@@ -0,0 +1,14 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+ 
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 300,
+    moveSlides: 1
+  });
+});
+</script>
+<!-- end Simple Custom CSS and JS -->
